0f063f4db5db480c0314b032278d3491734378fa
RoccoRichie <richieloughran@gmail.com>
2022-05-13 21:29:20 +0100

    Added second go test

--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -15,3 +15,12 @@ func TestHelloName(t *testing.T) {
 		t.Fatalf(`Hello("Bo") = %q, %v, want match for %#q, nil`, msg, err, want)
 	}
 }
+
+// TestHelloEmpty calls greetings.Hello with an empty string,
+// checking for an error
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hllo("" = %q, %v, want "", error`, msg, err)
+	}
+}
